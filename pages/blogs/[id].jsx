f68f9270cd0531cc8077ae8c20e4204da258a418
ariand <mriskiariandaa@gmail.com>
2022-08-21 13:21:28 +0700

    latihan 2: membuat template-section

--- pages/blogs/[id].jsx
+++ pages/blogs/[id].jsx
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router'
 import Link from 'next/link'
-const BlogDetail = ({data}) => {
 
+const BlogDetail = ({data}) => {
     const { id } = useRouter().query;
     return (
         <>
--- pages/blogs/index.jsx
+++ pages/blogs/index.jsx
@@ -1,16 +1,17 @@
+import Layout from '@/layouts/Layout'
+import Item from '@/components/Section/Item'
+
 import { useState, useEffect } from 'react'
 import Link from 'next/link'
 
-
 const Blog = ({getData_SSR}) => {
-    const [blogs, setBlogs] = useState([])
-    const [oke, setOke] = useState("oke")
+    const [blogs, setBlogs] = useState([])    
 
     useEffect(() => {        
         (async () => {
-            const getData = await fetch('https://jsonplaceholder.typicode.com/posts');
-            const data = await getData.json();
-            setBlogs(data);
+            // const getData = await fetch('https://jsonplaceholder.typicode.com/posts');
+            // const data = await getData.json();
+            setBlogs(getData_SSR);
         })()        
       }, []);
 
@@ -24,7 +25,11 @@ const Blog = ({getData_SSR}) => {
     const jumlahPost = blogs.length
     return (
         <>
-            <h2>Data 1</h2>
+            <Layout>                
+                
+                 {/*<h2 className="text-3xl font-bold underline">
+                    Data 1
+                </h2>
             <h3>Jumlah Data= {jumlahPost}</h3>
             <ul>
                 {blogs && blogs.map((data, index) => {
@@ -39,24 +44,24 @@ const Blog = ({getData_SSR}) => {
                         )    
                     }                    
                 )}
-            </ul>
+            </ul> */}
+            
+                <section className="bg-white border-b py-8">
+                    <div className="container mx-auto flex flex-wrap pt-4 pb-8">
+                        <h2 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800 pt-12">
+                            Jumlah Article {jumlahPost}
+                        </h2>
+                        <div className="w-full mb-4">
+                            <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+                        </div>
+                        {blogs.map((getData_SSR, index) => (
 
-            <h2>Data 2</h2>
-            <h4>Jumlah Data= {jumlahPost}</h4>
-            <ul>
-                {blogs && blogs.map((getData_SSR, index) => {
-                    return (                                            
-                        <div key={getData_SSR.id}>   
-                            <li>{getData_SSR.title}</li>
-                            <Link href={`blogs/${getData_SSR.id}`} >
-                                <button>Detail</button>                                                                
-                            </Link>
-                            <button onClick={() => deletePost(getData_SSR)} id={getData_SSR.id}>DELETE</button>
-                        </div>                                              
-                        )    
-                    }                    
-                )}
-            </ul>
+                            <Item post={getData_SSR} key={index}/>
+
+                        ))}
+                    </div>
+                </section>
+            </Layout>
         </>
     )
 }
@@ -64,7 +69,7 @@ const Blog = ({getData_SSR}) => {
 
 export async function getServerSideProps(context) {
     // Fetch data from external API
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`)
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
     const getData_SSR = await res.json()
     console.log(context);
     // Pass data to the page via props
